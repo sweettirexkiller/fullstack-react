d3ea30fe09c10f8b80454469baad545e7eda7510
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-05 18:59:15 +0200

    structure refactoring, comments, morgan logger, and mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const createError = require('http-errors');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const morganLogger = require('morgan');
+
+// Configuring strategies for authentication
 require('./services/passport');
 
+// App initialization
 const app = express();
 
+// Terminal incoming requests logger
+app.use(morganLogger('dev'));
+
+// Database connection
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {useMongoClient: true});
+
+// Routes for authentication
 require('./routes/authRoutes')(app);
 
+
+// Server port activation
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+});
