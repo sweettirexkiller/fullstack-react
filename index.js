f95dc15fa5b12657ca382fe5e6f54a7b77111ea3
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-05 13:01:43 +0200

    accestoken

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken)=>{
+   console.log(accessToken);
 }));
 
 const PORT = process.env.PORT || 5000;
