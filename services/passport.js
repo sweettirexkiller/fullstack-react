777ba722e717b219f1db02111fdb22f61161d219
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-05 19:31:34 +0200

    oauth google complete

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done)=>{
-    console.log('access token: ',accessToken);
-    console.log('refresh token: ',refreshToken);
-    console.log('profile: ',profile);
+}, async (accessToken, refreshToken, profile, done) => {
+    const user = await User.findOne({googleId: profile.id});
+    if (user) {
+        done(null, user);
+    } else {
+        await new User({googleId: profile.id, displayName: profile.displayName}).save();
+        done(null, user)
+    }
 }));
