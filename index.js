39aecfaf24ac7fb1538693e2449814edef2d8939
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-05 12:29:53 +0200

    staring cofnig of google passport strategy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'buddy'});
-});
+passport.use(new GoogleStrategy());
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
