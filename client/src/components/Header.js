12c49f0cfd58b650d84a337a28e030e41c3f3908
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-06 16:26:01 +0200

    landing component and header logo link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class Header extends Component {
-    renderContent(){
-        switch(this.props.auth){
+    renderContent() {
+        switch (this.props.auth) {
             case null:
                 return;
             case false:
@@ -16,13 +17,17 @@ class Header extends Component {
                 );
         }
     };
-    render(){
-        return(
+
+    render() {
+        return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">
+                    <Link
+                        to={!!this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
                         Emaily
-                    </a>
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
@@ -31,7 +36,9 @@ class Header extends Component {
         );
     }
 }
-function mapStateToProps({auth}){
+
+function mapStateToProps({auth}) {
     return {auth}
 }
+
 export default connect(mapStateToProps)(Header);
