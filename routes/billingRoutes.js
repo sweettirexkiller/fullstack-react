8727846410adaeb58c3351c2c41bb92efb3c307b
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-08 13:18:58 +0200

    auth redirection and delete comments

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,8 +2,12 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const User = require('../models/user');
 
+
 module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
+        if(!req.user){
+            return res.status(401).send({error: 'You must log in!'});
+        }
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
