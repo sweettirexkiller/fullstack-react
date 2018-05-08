5546d930906fd9920f3f710c3474ecf3d7f6ee64
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-08 15:38:39 +0200

    schema with sub documents

--- models/survey.js
+++ models/survey.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const recipientSchema = require('./recipient');
 
 const schema = new Schema({
     title: String,
     subject: String,
     body: String,
-    recipients: [String],
+    recipients: [recipientSchema],
+    yes: {type: Number, default: 0},
+    no: {type: Number, default: 0},
+    _user: {type: Schema.Types.ObjectId, ref: 'User'},
+    dateSent: Date,
+    lastResponded: Date
 });
 
 module.exports = mongoose.model('Surveys', schema);
