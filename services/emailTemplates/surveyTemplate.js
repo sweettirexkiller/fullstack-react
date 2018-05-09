3e8e65e96c3d785f63f6ab17f7b2b32f5da89207
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-09 13:08:11 +0200

    survey is being sent

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -8,7 +8,7 @@ module.exports = (survey) => {
                 <h3>I'd like your input!</h3>
                 <p>Please answer the following question:</p>
                 <p>${survey.body}</p>
-                <div><a href="${keys.redirectDomain}/api/survey/thanks">Yes</a></div>
+                <div><a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a></div>
                 <div><a href="${keys.redirectDomain}/api/surveys/thanks">No</a></div>
             </div>
         </body>
