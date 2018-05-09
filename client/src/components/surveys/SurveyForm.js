0e999b8b86c6722cbbf42db5b5b5edf933bbf881
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-09 12:03:35 +0200

    finished validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
 import SurveyField from './SurveyField';
 import {Link} from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
     {label: 'Survey Title', name: 'title'},
@@ -41,13 +42,13 @@ class SurveyForm extends Component {
 function validate(values){
     const errors = {};
 
+    errors.emails = validateEmails(values.emails || '');
+
     _.each(FIELDS, ({name})=>{
         if(!values[name]){
             errors[name] = `You must provide a value.`
         }
     });
-
-    
 
 
     return errors;
